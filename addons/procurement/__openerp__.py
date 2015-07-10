Odew/odoo-1

[IMP] stock,procurement : stat button procurement, ux improvements

addons/procurement/__openerp__.py
@@ -55,6 +55,7 @@ Procurements in exception should be checked manually and can be re-run.
         'wizard/schedulers_all_view.xml',
         'procurement_view.xml',
         'company_view.xml',
+        'product_view.xml',
     ],
     'demo': [],
     'test': ['test/procurement.yml'],

M4GiK/kamserp-v10

addons/procurement/__openerp__.py
@@ -37,6 +37,7 @@ Procurements in exception should be checked manually and can be re-run.
         'wizard/schedulers_all_view.xml',
         'procurement_view.xml',
         'company_view.xml',
+        'product_product_view.xml',
     ],
     'demo': [],
     'test': ['test/procurement.yml'],