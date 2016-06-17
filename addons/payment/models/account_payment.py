Bharat2512/odoo

[IMP] payment: create and use an electronic account.payment.method

addons/payment/models/account_payment.py
@@ -15,8 +15,17 @@ class AccountPayment(models.Model):
 
         return res
 
+    @api.onchange('payment_method_id', 'journal_id')
+    def _clear_electronic_payment_token(self):
+        if self.payment_method_id.code == 'electronic':
+            self.electronic_payment_token_id = self.env['payment.token'].search([('partner_id', '=', self.partner_id.id)], limit=1)
+        else:
+            self.electronic_payment_token_id = False
+
     payment_transaction_id = fields.Many2one('payment.transaction', string="Payment Transaction")
     electronic_payment_token_id = fields.Many2one('payment.token')
+    payment_type = fields.Selection(selection_add=[('electronic', 'Electronically receive money')])
+    payment_method_id_code = fields.Char(related='payment_method_id.code')
 
     def _do_payment(self):
         tx_obj = self.env['payment.transaction']
@@ -37,7 +46,7 @@ class AccountPayment(models.Model):
 
         s2s_result = tx.s2s_do_transaction()
 
-        if not s2s_result or tx.state != 'done': # todo jov: what about pending transactions
+        if not s2s_result or tx.state != 'done':
             raise ValidationError(_("Payment transaction failed (%s)") % tx.state_message)
 
         self.payment_transaction_id = tx
@@ -50,4 +59,3 @@ class AccountPayment(models.Model):
             res._do_payment()
 
         return res
-

M4GiK/kamserp-v10

addons/payment/models/account_payment.py
@@ -15,8 +15,17 @@ class AccountPayment(models.Model):
 
         return res
 
+    @api.onchange('payment_method_id', 'journal_id')
+    def _clear_electronic_payment_token(self):
+        if self.payment_method_id.code == 'electronic':
+            self.payment_token_id = self.env['payment.token'].search([('partner_id', '=', self.partner_id.id)], limit=1)
+        else:
+            self.payment_token_id = False
+
     payment_transaction_id = fields.Many2one('payment.transaction', string="Payment Transaction")
     payment_token_id = fields.Many2one('payment.token', string="Saved payment token")
+    payment_type = fields.Selection(selection_add=[('electronic', 'Electronically receive money')])
+    payment_method_id_code = fields.Char(related='payment_method_id.code')
 
     def _do_payment(self):
         tx_obj = self.env['payment.transaction']
@@ -37,7 +46,7 @@ class AccountPayment(models.Model):
 
         s2s_result = tx.s2s_do_transaction()
 
-        if not s2s_result or tx.state != 'done': # todo jov: what about pending transactions
+        if not s2s_result or tx.state != 'done':
             raise ValidationError(_("Payment transaction failed (%s)") % tx.state_message)
 
         self.payment_transaction_id = tx
@@ -50,4 +59,3 @@ class AccountPayment(models.Model):
             res._do_payment()
 
         return res
-