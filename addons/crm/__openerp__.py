Bharat2512/odoo

[MOV] crm : reoganize module directory and files

addons/crm/__openerp__.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-
 {
     'name': 'CRM',
     'version': '1.0',
@@ -42,48 +41,47 @@ Dashboard for CRM will include:
         'web_planner',
     ],
     'data': [
-        'data/crm_action_data.xml',
-        'crm_data.xml',
+        'data/web_tip_data.xml',
+        'data/crm_activity_data.xml',
+        'data/crm_data.xml',
         'data/crm_stage_data.xml',
         'data/sales_config_settings_data.xml',
-        'crm_lead_data.xml',
-        'crm_tip_data.xml',
+        'data/crm_lead_data.xml',
+        'data/web_planner_data.xml',
+        'data/mail_template_data.xml',
 
         'security/crm_security.xml',
         'security/ir.model.access.csv',
 
         'wizard/crm_activity_log_views.xml',
-        'wizard/crm_lead_lost_view.xml',
-        'wizard/crm_lead_to_opportunity_view.xml',
-        'wizard/crm_merge_opportunities_view.xml',
+        'wizard/crm_lead_lost_views.xml',
+        'wizard/crm_lead_to_opportunity_views.xml',
+        'wizard/crm_merge_opportunities_views.xml',
 
-        'crm_view.xml',
-        'crm_stage_views.xml',
-        'crm_lead_view.xml',
-        'crm_lead_menu.xml',
-        'views/crm_action_views.xml',
+        'views/crm_templates.xml',
+        'views/crm_views.xml',
+        'views/crm_activity_views.xml',
+        'views/crm_stage_views.xml',
+        'views/crm_lead_views.xml',
+        'crm_lead_menu.xml',  # TODO : move into crm_lead_views.xml
+        'views/crm_activity_views.xml',
+        'views/calendar_views.xml',
+        'views/res_partner_views.xml',
+        'views/res_config_views.xml',
 
-        'calendar_event_menu.xml',
+        'wizard/base_partner_merge_views.xml',
 
-        'report/crm_activity_report_view.xml',
-        'report/crm_opportunity_report_view.xml',
+        'report/crm_activity_report_views.xml',
+        'report/crm_opportunity_report_views.xml',
 
-        'res_partner_view.xml',
-
-        'res_config_view.xml',
-        'base_partner_merge_view.xml',
-
-        'sales_team_view.xml',
-        'views/crm.xml',
-        'web_planner_data.xml',
-        'sales_team_dashboard.xml',
-        'crm_mail_template_data.xml'
+        'views/crm_team_views.xml',
+        'sales_team_dashboard.xml',  # TODO : merge with crm_Team_views
     ],
     'demo': [
-        'crm_demo.xml',
-        'crm_lead_demo.xml',
-        'data/crm_action_demo.xml',
-        'crm_action_rule_demo.xml',
+        'data/crm_demo.xml',
+        'data/crm_lead_demo.xml',
+        'data/crm_activity_demo.xml',
+        'data/base_action_rule_demo.xml',
     ],
     'test': [
         'test/crm_access_group_users.yml',

M4GiK/kamserp-v10

addons/crm/__openerp__.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-
 {
     'name': 'CRM',
     'version': '1.0',
@@ -42,48 +41,47 @@ Dashboard for CRM will include:
         'web_tour',
     ],
     'data': [
-        'data/crm_action_data.xml',
-        'crm_data.xml',
+        'data/tour_data.xml',
+        'data/crm_activity_data.xml',
+        'data/crm_data.xml',
         'data/crm_stage_data.xml',
         'data/sales_config_settings_data.xml',
-        'crm_lead_data.xml',
+        'data/crm_lead_data.xml',
+        'data/web_planner_data.xml',
+        'data/mail_template_data.xml',
 
         'security/crm_security.xml',
         'security/ir.model.access.csv',
 
         'wizard/crm_activity_log_views.xml',
-        'wizard/crm_lead_lost_view.xml',
-        'wizard/crm_lead_to_opportunity_view.xml',
-        'wizard/crm_merge_opportunities_view.xml',
+        'wizard/crm_lead_lost_views.xml',
+        'wizard/crm_lead_to_opportunity_views.xml',
+        'wizard/crm_merge_opportunities_views.xml',
 
-        'crm_view.xml',
-        'crm_stage_views.xml',
-        'crm_lead_view.xml',
-        'crm_lead_menu.xml',
-        'views/crm_action_views.xml',
+        'views/crm_templates.xml',
+        'views/crm_views.xml',
+        'views/crm_activity_views.xml',
+        'views/crm_stage_views.xml',
+        'views/crm_lead_views.xml',
+        'crm_lead_menu.xml',  # TODO : move into crm_lead_views.xml
+        'views/crm_activity_views.xml',
+        'views/calendar_views.xml',
+        'views/res_partner_views.xml',
+        'views/res_config_views.xml',
 
-        'calendar_event_menu.xml',
+        'wizard/base_partner_merge_views.xml',
 
-        'report/crm_activity_report_view.xml',
-        'report/crm_opportunity_report_view.xml',
+        'report/crm_activity_report_views.xml',
+        'report/crm_opportunity_report_views.xml',
 
-        'res_partner_view.xml',
-
-        'res_config_view.xml',
-        'base_partner_merge_view.xml',
-
-        'sales_team_view.xml',
-        'views/crm.xml',
-        'web_planner_data.xml',
-        'sales_team_dashboard.xml',
-        'crm_mail_template_data.xml',
-        'tour_view.xml'
+        'views/crm_team_views.xml',
+        'sales_team_dashboard.xml',  # TODO : merge with crm_Team_views
     ],
     'demo': [
-        'crm_demo.xml',
-        'crm_lead_demo.xml',
-        'data/crm_action_demo.xml',
-        'crm_action_rule_demo.xml',
+        'data/crm_demo.xml',
+        'data/crm_lead_demo.xml',
+        'data/crm_activity_demo.xml',
+        'data/base_action_rule_demo.xml',
     ],
     'test': [
         'test/crm_access_group_users.yml',